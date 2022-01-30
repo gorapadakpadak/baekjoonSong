9b6713be978733e1771104ed5e3a4f74e08d03c7
chaewonii <sea9392@gmail.com>
2022-01-30 22:20:04 +0900

    <1/31~2/3 설 연휴> 새해 복 많이 받으세요!

---
+++ Week4/설연휴.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static int n;
+    public static int[] A;
+    public static char[][] S;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("새해 복 많이 받으세요!!");
+    }
+}
+
+
