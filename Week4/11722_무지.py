3b0eb5cc60303cb92dad4aa2d53e60e4741b9c7d
Suah Kang <christinekang0123@naver.com>
2022-02-05 23:35:54 +0900

    0205(토)_가장 긴 감소하는 부분 수열

---
+++ Week4/11722_무지.py
@@ -0,0 +1,18 @@
+import sys 
+input = lambda : sys.stdin.realine().strip()
+
+n = int(input())
+a = list(map(int, input().split()))
+dp = [1 for i in range(n)]
+
+for i in range(1, n):
+    s = []
+    for j in range(i):
+        if a[i] < a[j]:
+            s.append(dp[j])
+    if not s:
+        continue
+    else:
+        dp[i] += max(s)
+        
+print(max(dp))
