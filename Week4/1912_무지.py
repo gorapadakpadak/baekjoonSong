510e2fb1936c96dc50a125766eca931ba9cd14c2
Suah Kang <christinekang0123@naver.com>
2022-02-06 23:25:48 +0900

    0206(일)_연속합

---
+++ Week4/1912_무지.py
@@ -0,0 +1,11 @@
+import sys 
+input = lambda : sys.stdin.readline().strip()
+
+n = int(input())
+a = list(map(int, input().split()))
+sum = [a[0]]
+
+for i in range(len(a) - 1):
+    sum.append(max(sum[i] + a[i + 1], a[i + 1]))
+
+print(max(sum))
